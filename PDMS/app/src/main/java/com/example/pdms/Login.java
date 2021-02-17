8a2d0c7df74de1877f4dcd99e02b86c90b1d9891
chthao123 <63527673+chthao123@users.noreply.github.com>
2021-02-16 18:00:57 -0800

    Patient Profile
    -Started Patient Profile
    -place layout

--- PDMS/app/src/main/java/com/example/pdms/Login.java
+++ PDMS/app/src/main/java/com/example/pdms/Login.java
@@ -15,7 +15,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
     EditText et_email, et_password;
@@ -84,4 +88,5 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
 }
